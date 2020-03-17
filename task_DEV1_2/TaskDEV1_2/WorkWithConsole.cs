4aa1f99c7dbe615e478fb87356987c4dad7026fc
Kseniya Tarasevich <tarasevichkseniya11@gmail.com>
2020-03-17 21:05:10 +0300

    added comments

--- task_DEV1_2/TaskDEV1_2/WorkWithConsole.cs
+++ task_DEV1_2/TaskDEV1_2/WorkWithConsole.cs
@@ -2,12 +2,18 @@
 
 namespace TaskDEV1_2
 {
+    /// <summary>
+    /// Class for working with console 
+    /// </summary>
     class WorkWithConsole
     {
         string consoleNumber;
         string consoleSystemBase;
         ConverterIntToAnotherNumeralSystem converter = new ConverterIntToAnotherNumeralSystem();
 
+        /// <summary>
+        /// Method for input base of a system and number
+        /// </summary>
         public void ConsoleInput()
         {
             Console.Write("Enter your number in decimal system: ");
@@ -16,6 +22,9 @@ namespace TaskDEV1_2
             consoleSystemBase = Console.ReadLine();
         }
 
+        /// <summary>
+        /// Method for output number in other numeral system
+        /// </summary>
         public void ConsoleOutput()
         {
             string answer;
