cd5f2cbe546567806a167ad771bae9ba9a663e1e
Kseniya Tarasevich <tarasevichkseniya11@gmail.com>
2020-03-27 01:33:04 +0300

    homework 1

---
+++ hw1/task_HW1/Teacher.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace task_HW1
+{
+    class Teacher
+    {
+        public Teacher()
+        {
+            TaskResults = new List<string>();
+        }
+
+        public List<string> TaskResults { get; private set; }
+
+        public void AcceptTheWork(Student student, string answer)
+        {
+            TaskResults.Add(student.StudentName + " " + student.StudentSurname + " " + answer);
+
+            if (TaskResults.Count == Student.MAX_COUNT)
+            {
+                PrintStudentListToConsole();
+            }
+        }
+
+        public void StudentWorkDone(Student student)
+        {
+            student.Notification += AcceptTheWork;
+        }
+
+        public void PrintStudentListToConsole()
+        {
+            foreach (string str in TaskResults)
+            {
+                Console.WriteLine(str);
+            }
+        }
+    }
+}
